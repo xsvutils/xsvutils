9110b299092d0267c5fbd5e4f8a7f70d8f85f8c6
suzuki-navi <suzuki.navi.dev@gmail.com>
2018-01-03 21:38:46 +0900

    buffer.go のCPU 100% の対策

--- golang/lib/buffer/buffer.go
+++ golang/lib/buffer/buffer.go
@@ -13,11 +13,26 @@ func Buffer() {
 		fmt.Fprintln(os.Stderr, "epoll_create:", err)
 		os.Exit(1)
 	}
+	epfd1, err := syscall.EpollCreate1(0)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "epoll_create:", err)
+		os.Exit(1)
+	}
+	epfd2, err := syscall.EpollCreate1(0)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "epoll_create:", err)
+		os.Exit(1)
+	}
 
 	{
 		var event syscall.EpollEvent
 		event.Events = syscall.EPOLLIN
 		err := syscall.EpollCtl(epfd, syscall.EPOLL_CTL_ADD, 0, &event);
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		err = syscall.EpollCtl(epfd1, syscall.EPOLL_CTL_ADD, 0, &event);
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -31,10 +46,17 @@ func Buffer() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+		err = syscall.EpollCtl(epfd2, syscall.EPOLL_CTL_ADD, 1, &event);
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 
 	var stdin_closed bool = false;
 	var stdout_closed bool = false;
+	var stdin_ok bool = false;
+	var stdout_ok bool = false;
 
 	var buffer []byte = make([]byte, 40 * 1024 * 1024);
 	var offset1 int = 0;
@@ -46,32 +68,61 @@ func Buffer() {
 		}
 
 		var events [2]syscall.EpollEvent
-		nevents, err := syscall.EpollWait(epfd, events[:], -1)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+		var nevents int
+		var err error
+		if stdin_ok {
+			nevents, err = syscall.EpollWait(epfd2, events[:], -1)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		} else if stdout_ok {
+			nevents, err = syscall.EpollWait(epfd1, events[:], -1)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		} else {
+			nevents, err = syscall.EpollWait(epfd, events[:], -1)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
 		}
 
 		for i := 0; i < nevents; i++ {
 			if (events[i].Events & syscall.EPOLLIN) != 0 {
-				nbytes, err := syscall.Read(0, buffer[offset1:])
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "syscall.Read:", err)
-					os.Exit(1)
+				if offset1 == len(buffer) {
+					stdin_ok = true
+				} else {
+					stdout_ok = false
+
+					nbytes, err := syscall.Read(0, buffer[offset1:])
+					if err != nil {
+						fmt.Fprintln(os.Stderr, "syscall.Read:", err)
+						os.Exit(1)
+					}
+					offset1 += nbytes
 				}
-				offset1 += nbytes
 
 			} else if (events[i].Events & syscall.EPOLLOUT) != 0 {
-				nbytes, err := syscall.Write(1, buffer[offset2:offset1])
-				if err != nil {
-					os.Exit(0) // 出力先がなくなった場合はそのまま終了する
-				}
-				offset2 += nbytes
-				if offset1 == offset2 {
-					offset1 = 0
-					offset2 = 0
-					if stdin_closed {
-						stdout_closed = true
+				if offset2 == offset1 {
+					stdout_ok = true
+				} else {
+					stdin_ok = false
+
+					nbytes, err := syscall.Write(1, buffer[offset2:offset1])
+					if err != nil {
+						os.Exit(0) // 出力先がなくなった場合はそのまま終了する
+					}
+					offset2 += nbytes
+
+					if offset1 == offset2 {
+						offset1 = 0
+						offset2 = 0
+						if stdin_closed {
+							stdout_closed = true
+						}
 					}
 				}
 
