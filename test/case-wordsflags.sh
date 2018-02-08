c1db11abde730dc2beb6ff80d73ba41515e0e68e
suzuki-navi <suzuki.navi.dev@gmail.com>
2018-02-08 20:25:29 +0900

    add test case of wordsflags

---
+++ test/case-wordsflags.sh
@@ -0,0 +1,3 @@
+
+./xsvutils ./test/data/sample-wordsflags.tsv wordsflags false=f true=t > ./var/test/case-wordsflags-1.tsv
+
--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,7 @@ bash ./test/case-uriparams.sh
 bash ./test/case-bom.sh
 bash ./test/case-facetcount.sh
 bash ./test/case-crosstable.sh
+bash ./test/case-wordsflags.sh
 bash ./test/case-addmap.sh
 
 diff -ru ./test/expected ./var/test && echo OK
