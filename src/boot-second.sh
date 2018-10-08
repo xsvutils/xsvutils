90259a12550e87582343b74c98fcf42c400e6436
suzuki-navi <suzuki.navi.dev@gmail.com>
2018-10-08 11:28:52 +0900

    create src/boot-second.sh

---
+++ src/boot-second.sh
@@ -0,0 +1,24 @@
+
+if [ -z "$UID" ]; then
+    UID=$(id -u)
+fi
+if [ -d /run/user/$UID ]; then
+    export WORKING_DIR=$(mktemp -d /run/user/$UID/xsvutils-XXXXXXXX)
+elif [ -d /dev/shm ]; then
+    export WORKING_DIR=$(mktemp -d /dev/shm/xsvutils-XXXXXXXX)
+else
+    export WORKING_DIR=$(mktemp -d /tmp/xsvutils-XXXXXXXX)
+fi
+[ -n "$WORKING_DIR" ] || { echo "Cannot create WORKING_DIR: $WORKING_DIR"; exit $?; }
+
+export HARD_WORKING_DIR=$(mktemp -d /tmp/xsvutils-hard-XXXXXXXX)
+[ -n "$HARD_WORKING_DIR" ] || { echo "Cannot create HARD_WORKING_DIR: $HARD_WORKING_DIR"; exit $?; }
+
+trap "rm -rf $WORKING_DIR $HARD_WORKING_DIR; perl $TOOL_DIR/killchildren.pl $$" EXIT
+
+tput lines >/dev/null 2>&1 && export TERMINAL_LINES=$(tput lines);
+
+perl $TOOL_DIR/main.pl "$@"
+
+exit $?
+
