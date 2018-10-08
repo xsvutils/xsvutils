c04cefb49fe4b09b6c8b58d0fee3cfa9ee3f26a3
suzuki-navi <suzuki.navi.dev@gmail.com>
2018-10-08 18:04:01 +0900

    improve etc/install-golang.sh

--- etc/install-golang.sh
+++ etc/install-golang.sh
@@ -4,28 +4,31 @@
 
 VERSION=1.9.2
 
-if [ -e var/golang -a  -e var/golang_packages ]; then
-    exit 0
-fi
-
 cd var
 
-if [ ! -e go$VERSION.linux-amd64.tar.gz ]; then
-    url=https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
-    echo curl $url \> go$VERSION.linux-amd64.tar.gz
-    curl $url > go$VERSION.linux-amd64.tar.gz.tmp || exit $?
-    mv go$VERSION.linux-amd64.tar.gz.tmp go$VERSION.linux-amd64.tar.gz
+fname=golang-$VERSION.linux-amd64
+
+if [ ! -e $fname ]; then
+    if [ ! -e $fname.tar.gz ]; then
+        url=https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
+        echo curl -L $url \> $fname.tar.gz
+        curl -L $url > $fname.tar.gz.tmp || exit $?
+        mv $fname.tar.gz.tmp $fname.tar.gz
+    fi
+
+    if [ -e go ]; then
+        echo "directory `var/go` exists" >&2
+        exit 1
+    fi
+    echo tar xzf $fname.tar.gz
+    tar xzf $fname.tar.gz || exit $?
+    mv go $fname || exit $?
 fi
-if [ -e go ]; then
-    echo "directory `var/go` exists" >&2
-    exit 1
+
+if [ -e golang ]; then
+    rm golang
 fi
-if [ ! -e go$VERSION.linux-amd64 ]; then
-    echo tar xzf go$VERSION.linux-amd64.tar.gz
-    tar xzf go$VERSION.linux-amd64.tar.gz || exit $?
-    mv go go$VERSION.linux-amd64 || exit $?
-fi
-ln -s go$VERSION.linux-amd64 golang || exit $?
+ln -s $fname golang || exit $?
 
 mkdir -pv golang_packages
 
