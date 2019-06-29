6a8f55e45eeae7268ca6e991c942173ee584a84a
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-06-29 14:49:11 +0900

    fix failure downloading sbt

--- src/install.sh
+++ src/install.sh
@@ -4,35 +4,27 @@ recursive_flag=$2
 if [ "$version" = -v3 ]; then
     # version 0.2.7
     GIT_HASH=f1dfb7755c26eca2a99ad5786eac9c2e5e62caf2
-    make_arg=build
 elif [ "$version" = -v4 ]; then
     # version 0.4
     GIT_HASH=c1607ae08017aeec7983faaa3be8ec71ce3eea77
-    make_arg=build
 elif [ "$version" = -v5 ]; then
     # version 0.5
     GIT_HASH=610ca476f94ccfea76f2f0879c63781535c05285
-    make_arg=build
 elif [ "$version" = -v6 ]; then
     # version 0.6
     GIT_HASH=5ee4226daac842c4d9d5a364b06128238503c164
-    make_arg=build
 elif [ "$version" = -v7 ]; then
     # version 0.7
     GIT_HASH=8456d18517212e7f1e2f19c4bff75b94b18bb134
-    make_arg=build
 elif [ "$version" = -v8 ]; then
     # version 0.8
     GIT_HASH=0.8
-    make_arg=build
 elif [ "$version" = -v9 ]; then
     # version 0.9
     GIT_HASH=0.9
-    make_arg=build
 elif [ "$version" = -v10 ]; then
     # version 0.10
     GIT_HASH=0.10
-    make_arg=build
 elif [ -n "$recursive_flag" ]; then
     echo "Unknown version: $version" >&2
     exit 1
@@ -75,7 +67,13 @@ if [ ! -e $BUILD_DIR/var/xsvutils$version ]; then
         cd $BUILD_DIR
         git fetch --prune
         git checkout -q $GIT_HASH || exit $?
-        make $make_arg || exit $?
+
+        make var/makefile || exit $?
+
+        # ダウンロードURLが変わってしまったためにパッチ
+        cat var/makefile | sed 's#https://cocl.us/sbt-0.13.16.tgz#https://piccolo.link/sbt-0.13.16.tgz#g' > var/makefile.2
+        make -f var/makefile.2 build || exit $?
+
         cp xsvutils var/xsvutils$version
     ) || exit $?
 fi
