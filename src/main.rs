1b098c4cb4d57ec282bde45575984825996d4754
irxground <irxnjhtchlnrw@gmail.com>
2019-07-03 15:09:09 +0900

    Rust にコマンドラインパーサーを導入

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
 #[macro_use]
 mod util;
 
-use std::env;
 use std::io;
 use std::io::BufWriter;
+mod addcopy;
 mod command;
 mod cut;
 mod uriparams;
-mod addcopy;
 
-fn run<C: command::Command>(rest: Vec<String>) {
+use structopt::*;
+
+#[derive(StructOpt, Debug)]
+enum Opt {
+    #[structopt(name = "addcopy")]
+    AddCopy(addcopy::Opt),
+
+    #[structopt(name = "cut")]
+    Cut(cut::Opt),
+
+    #[structopt(name = "uriparams")]
+    UriParams(uriparams::Opt),
+}
+
+fn main() {
+    let opt = Opt::from_args();
     let stdin = io::stdin();
     let mut stdin = stdin.lock();
     let stdout = io::stdout();
     let stdout = stdout.lock();
     let mut stdout = BufWriter::new(stdout);
-    let _ = C::execute(rest, &mut stdin, &mut stdout);
-}
 
-fn main() {
-    let mut argv = env::args();
-    let _ = argv.next();
-    let sub_command = argv.next();
-    let rest: Vec<_> = argv.collect();
-
-    if let Some(sub_command) = sub_command {
-        match sub_command.as_str() {
-            "cut" => run::<cut::CutCommand>(rest),
-            "addcopy" => run::<addcopy::AddCopyCommand>(rest),
-            "uriparams" => run::<uriparams::UriParamsCommand>(rest),
-            _ => die!("unknown subcommand: {}", &sub_command),
-        }
-    } else {
-        die!("subcommand is not specified");
-    }
+    let _ = match opt {
+        Opt::AddCopy(x) => addcopy::run(x, &mut stdin, &mut stdout),
+        Opt::Cut(x) => cut::run(x, &mut stdin, &mut stdout),
+        Opt::UriParams(x) => uriparams::run(x, &mut stdin, &mut stdout),
+    };
 }
