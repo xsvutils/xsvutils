d4029ade8f6e9dc468348d880a828e942554efdf
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-09-29 13:05:48 +0900

    fix a bug of list-help.sh

--- etc/list-help.sh
+++ etc/list-help.sh
@@ -4,7 +4,7 @@ action=$1
 
 if [ "$action" = "all" ]; then
     ls src/*-help.txt | sed -E 's#^src/(.+)-help\.txt$#cmd-\1.txt#g'
-    ls src/help-*.txt | sed -E 's#^src/help-(.+)\.txt$#guide-\1.txt#g'
+    ls src/help-*.txt | grep -v -E '(main|notfound)' | sed -E 's#^src/help-(.+)\.txt$#guide-\1.txt#g'
     echo main.txt
     echo notfound.txt
 fi
